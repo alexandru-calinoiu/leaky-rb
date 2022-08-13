35d8a43876eb97597aa615fdadef3fbd99230bcf
calin <calinoiu.alexandru@agilefreaks.com>
2022-08-13 18:13:57 +0300

    See gc live object count

--- leaky.rb
+++ leaky.rb
@@ -1,7 +1,13 @@
+GC.disable
+
 an_array = []
 
 loop do
   1000.times { an_array << "A" + "B" + "C" }
   puts an_array.size
+
+  GC.start
+  puts "There are #{GC.stat(:heap_live_slots)} live objects"
+
   sleep 1
 end
